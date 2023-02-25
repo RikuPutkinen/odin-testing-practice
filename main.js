b3f838120ee2964deb76f74ed2569cfc3470ca1c
Riku Putkinen <putkinenriku@gmail.com>
2023-02-25 19:00:35 +0200

    Add function and tests for "calculator"

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ export function capitalize(str) {
 export function reverseString(str) {
   return str.split("").reverse().join("");
 }
+
+export const calculator = {
+  add: function(a, b) {
+    return a + b;
+  },
+
+  subtract: function(a, b) {
+    return a - b;
+  },
+
+  divide: function(a, b) {
+    if (b === 0) throw new Error("Cannot divide with 0");
+    return a / b;
+  },
+
+  multiply: function(a, b) {
+    return a * b;
+  }
+}
--- main.test.js
+++ main.test.js
@@ -1,4 +1,5 @@
-import { capitalize, reverseString } from "./main";
+import expect from "expect";
+import { capitalize, reverseString, calculator } from "./main";
 
 test('Capitalize the string', () => {
   expect(capitalize("aBc")).toBe("Abc");
@@ -6,4 +7,24 @@ test('Capitalize the string', () => {
 
 test('Reverse the string', () => {
   expect(reverseString("abc")).toBe("cba");
+});
+
+test('Add numbers', () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test('Subtract numbers', () => {
+  expect(calculator.subtract(3, 2)).toBe(1);
+});
+
+test('Divide numbers', () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test('Divide with zero', () => {
+  expect(() => calculator.divide(10, 0)).toThrow(/^Cannot divide with 0$/);
+});
+
+test('Multiply numbers', () => {
+  expect(calculator.multiply(5, 2)).toBe(10);
 });
